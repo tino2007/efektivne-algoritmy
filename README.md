cc98f15501d56cd56e7ea3c1f9e520acb74d9ed3
jbub <git@jbub.eu>
2014-04-22 09:24:55 +0200

    pridany zoznam uloh

--- README.md
+++ README.md
@@ -2,3 +2,55 @@ efektivne-algoritmy
 ===================
 
 Ludia posielajte okomentovane zdrojaky nie prezentacie a wordacke dokumenty.
+
+Zoznam
+------
+
+| Nazov                                     | Odkaz              | Obtiaznost  | Student           |
+| ----------------------------------------- | ------------------ | ----------- | ----------------- |
+| Erdos Numbers Miguel Revilla              | [110206](/110206/) | 2           | Jaroslav Beňo     |
+| Football (aka Soccer) Pedro Demasi        | [110408](/110408/) | 1           | Jaroslav Beňo     |
+| Counting Pedro Demasi                     | [110603](/110603/) | 2           | Matej Brašeň      |
+| Cutting Sticks Manuel Carro               | [111105](/111105/) | 2           | Matej Brašeň      |
+| Slash Maze Miguel Revilla                 | [110904](/110904/) | 2           | Martin Brzula     |
+| Playing With Wheels Shahriar Manzoor      | [110902](/110902/) | 2           | Martin Brzula     |
+| Contest Scoreboard Gordon Cormack         | [110207](/110207/) | 1           | Juraj Bubniak     |
+| Crypt Kicker Gordon Cormack               | [110204](/110204/) | 2           | Juraj Bubniak     |
+| Freckles Gordon Cormack                   | [111001](/111001/) | 2           | Ján Fekeš         |
+| CDVII Gordon Cormack                      | [110406](/110406/) | 2           | Ján Fekeš         |
+| How Many Fibs? Rujia Liu                  | [110601](/110601/) | 1           | Peter Franček     |
+| Summation of Four Primes Shahriar Manzoor | [110705](/110705/) | 2           | Peter Franček     |
+| Check the Check Pedro Demasi              | [110107](/110107/) | 1           | Ladislav Gubala   |
+| Interpreter Gordon Cormack                | [110106](/110106/) | 2           | Ladislav Gubala   |
+| Hanoi Tower Troubles Again! Rujia Liu     | [110908](/110908/) | 3           | Ivan Hraško       |
+| A Multiplication Game Gordon Cormack      | [110505](/110505/) | 3           | Ivan Hraško       |
+| Primary Arithmetic Gordon Cormack         | [110501](/110501/) | 1           | Pavel Jančiar     |
+| Hartals Shahriar Manzoor                  | [110203](/110203/) | 2           | Pavel Jančiar     |
+| Tug of War Gordon Cormack                 | [110805](/110805/) | 2           | Jakub Jurčík      |
+| Factovisors Gordon Cormack                | [110704](/110704/) | 2           | Jakub Jurčík      |
+| Crypt Kicker II Gordon Cormack            | [110304](/110304/) | 2           | Nikoleta Kabáčová |
+| File Fragmentation Gordon Cormack         | [110306](/110306/) | 2           | Nikoleta Kabáčová |
+| The 3n + 1 problem Owen Astrakan          | [110101](/110101/) | 1           | Peter Kajan       |
+| Stacks of Flapjacks Owen Astrakan         | [110402](/110402/) | 2           | Peter Kajan       |
+| Automated Judge Script Pedro Demasi       | [110305](/110305/) | 1           | Ján Maár          |
+| 15-Puzzle Problem Shahriar Manzoor        | [110802](/110802/) | 3           | Ján Maár          |
+| Self-describing Sequence Shahriar Manzoor | [110607](/110607/) | 2           | Matúš Marko       |
+| Dermuba Triangle Arun Kishore             | [111207](/111207/) | 2           | Matúš Marko       |
+| Light                                     | [110701](/110701/) | 1           | Jozef Medveď      |
+| Knights of the Round Table Pedro Demasi   | [111303](/111303/) | 2           | Jozef Medveď      |
+| The Trip Gordon Cormack                   | [110103](/110103/) | 1           | Tomáš Nguyen      |
+| Where’s Waldorf? Gordon Cormack           | [110302](/110302/) | 2           | Tomáš Nguyen      |
+| Stack ’em Up Gordon Cormack               | [110205](/110205/) | 1           | Matúš Novysedlák  |
+| Yahtzee Gordon Cormack                    | [110208](/110208/) | 3           | Matúš Novysedlák  |
+| Common Permutation Shahriar Manzoor       | [110303](/110303/) | 1           | Peter Otto        |
+| Shoemaker’s Problem Alex Gevak            | [110405](/110405/) | 2           | Peter Otto        |
+| Minesweeper Pedro Demasi                  | [110102](/110102/) | 1           | Radoslav Rajčan   |
+| Poker Hands Gordon Cormack                | [110202](/110202/) | 2           | Radoslav Rajčan   |
+| WERTYU Gordon Cormack                     | [110301](/110301/) | 1           | Matej Tepper      |
+| Steps Gordon Cormack                      | [110608](/110608/) | 2           | Matej Tepper      |
+| Reverse and Add Erick Moreno              | [110502](/110502/) | 1           | Ján Turský        |
+| Ones Gordon Cormack                       | [110504](/110504/) | 2           | Ján Turský        |
+| Jolly Jumpers Gordon Cormack              | [110201](/110201/) | 1           | Štefan Ušák       |
+| Rope Crisis in Ropeland! Shahriar Manzoor | [111302](/111302/) | 2           | Štefan Ušák       |
+| Carmichael Numbers Manuel Carro           | [110702](/110702/) | 2           | Martin Jančo      |
+| Euclid Problem Alexander Denisjuk         | [110703](/110703/) | 1           | Martin Jančo      |
---
+++ generate.py
@@ -0,0 +1,209 @@
+# coding=utf-8
+
+tasks = u'''
+1.1 110101 100 The 3n + 1 problem Owen Astrakan
+1.2 110102 10189 Minesweeper Pedro Demasi
+1.3 110103 10137 The Trip Gordon Cormack
+1.4 110104 706 LCD Display Miguel Revilla, Immanuel Herrman
+1.5 110105 10267 Graphical Editor Alexander Denisjuk
+1.6 110106 10033 Interpreter Gordon Cormack
+1.7 110107 10196 Check the Check Pedro Demasi
+1.8 110108 10142 Australian Voting Gordon Cormack
+2.1 110201 10038 Jolly Jumpers Gordon Cormack, Wim Nuij
+2.2 110202 10315 Poker Hands Gordon Cormack
+2.3 110203 10050 Hartals Shahriar Manzoor, Rezaul Alam Chowdhury
+2.4 110204 843 Crypt Kicker Gordon Cormack
+2.5 110205 10205 Stack ’em Up Gordon Cormack
+2.6 110206 10044 Erdos Numbers Miguel Revilla, Felix Gaertner
+2.7 110207 10258 Contest Scoreboard Gordon Cormack, Michael Van Biesbrouck
+2.8 110208 10149 Yahtzee Gordon Cormack
+3.1 110301 10082 WERTYU Gordon Cormack
+3.2 110302 10010 Where’s Waldorf? Gordon Cormack
+3.3 110303 10252 Common Permutation Shahriar Manzoor
+3.4 110304 850 Crypt Kicker II Gordon Cormack
+3.5 110305 10188 Automated Judge Script Pedro Demasi
+3.6 110306 10132 File Fragmentation Gordon Cormack, Charles Clarke
+3.7 110307 10150 Doublets Gordon Cormack
+3.8 110308 848 Fmt Gordon Cormack
+4.1 110401 10041 Vito’s Family Miguel Revilla, Pablo Puente
+4.2 110402 120 Stacks of Flapjacks Owen Astrakan
+4.3 110403 10037 Bridge Gordon Cormack
+4.4 110404 10191 Longest Nap Pedro Demasi
+4.5 110405 10026 Shoemaker’s Problem Alex Gevak, Antonio S´anchez
+4.6 110406 10138 CDVII Gordon Cormack
+4.7 110407 10152 ShellSort Gordon Cormack, Charles Clarke
+4.8 110408 10194 Football (aka Soccer) Pedro Demasi
+5.1 110501 10035 Primary Arithmetic Gordon Cormack
+5.2 110502 10018 Reverse and Add Erick Moreno
+5.3 110503 701 The Archeologist’s Dilemma Miguel Revilla
+5.4 110504 10127 Ones Gordon Cormack, Piotr Rudnicki
+5.5 110505 847 A Multiplication Game Gordon Cormack, Piotr Rudnicki
+5.6 110506 10105 Polynomial Coeﬃcients Alexander Denisjuk
+5.7 110507 10077 Stern-Brocot Number System Shahriar Manzoor, Rezaul Alam Chowdhury
+5.8 110508 10202 Pairsumonious Numbers Gordon Cormack, Piotr Rudnicki
+6.1 110601 10183 How Many Fibs? Rujia Liu, Walter Guttmann
+6.2 110602 10213 How Many Pieces of Land? Shahriar Manzoor
+6.3 110603 10198 Counting Pedro Demasi
+6.4 110604 10157 Expressions Petko Minkov
+6.5 110605 10247 Complete Tree Labeling Shahriar Manzoor
+6.6 110606 10254 The Priest Mathematician Shahriar Manzoor, Miguel Revilla
+6.7 110607 10049 Self-describing Sequence Shahriar Manzoor, Rezaul Alam Chowdhury
+6.8 110608 846 Steps Gordon Cormack, Piotr Rudnicki
+7.1 110701 10110 Light, More Light Udvranto Patik, Sadi Khan, Suman Mahbub
+7.2 110702 10006 Carmichael Numbers Manuel Carro, C´esar S´anchez
+7.3 110703 10104 Euclid Problem Alexander Denisjuk
+7.4 110704 10139 Factovisors Gordon Cormack
+7.5 110705 10168 Summation of Four Primes Shahriar Manzoor
+7.6 110706 10042 Smith Numbers Miguel Revilla, Felix Gaertner
+7.7 110707 10090 Marbles Shahriar Manzoor, Rezaul Alam Chowdhury
+7.8 110708 10089 Repackaging Shahriar Manzoor, Rezaul Alam ChowdhuryA.5. Problem Credits 349
+8.1 110801 861 Little Bishops Shahriar Manzoor, Rezaul Alam Chowdhury
+8.2 110802 10181 15-Puzzle Problem Shahriar Manzoor, Rezaul Alam Chowdhury
+8.3 110803 10128 Queue Marcin Wojciechowski
+8.4 110804 10160 Servicing Stations Petko Minkov
+8.5 110805 10032 Tug of War Gordon Cormack
+8.6 110806 10001 Garden of Eden Manuel Carro, Manuel J. Petit de Gabriel
+8.7 110807 704 Color Hash Miguel Revilla, Pablo Puente
+8.8 110808 10270 Bigger Square Please... Rujia Liu
+9.1 110901 10004 Bicoloring Manuel Carro, Alvaro Mart´ ´ ınez Echevarr´ıa
+9.2 110902 10067 Playing With Wheels Shahriar Manzoor, Rezaul Alam Chowdhury
+9.3 110903 10099 The Tourist Guide Shahriar Manzoor, Rezaul Alam Chowdhury
+9.4 110904 705 Slash Maze Miguel Revilla, Immanuel Herrman
+9.5 110905 10029 Edit Step Ladders Gordon Cormack
+9.6 110906 10051 Tower of Cubes Shahriar Manzoor, Rezaul Alam Chowdhury
+9.7 110907 10187 From Dusk Till Dawn Rujia Liu, Ralf Engels
+9.8 110908 10276 Hanoi Tower Troubles Again! Rujia Liu
+10.1 111001 10034 Freckles Gordon Cormack
+10.2 111002 10054 The Necklace Shahriar Manzoor, Rezaul Alam Chowdhury
+10.3 111003 10278 Fire Station Gordon Cormack
+10.4 111004 10039 Railroads Miguel Revilla, Philipp Hahn
+10.5 111005 10158 War Petko Minkov
+10.6 111006 10199 Tourist Guide Pedro Demasi
+10.7 111007 10249 The Grand Dinner Shahriar Manzoor, Rezaul Alam Chowdhury
+10.8 111008 10092 Problem With Problem Setter Shahriar Manzoor, Rezaul Alam Chowdhury
+11.1 111101 10131 Is Bigger Smarter? Gordon Cormack, Charles Rackoﬀ
+11.2 111102 10069 Distinct Subsequences Shahriar Manzoor, Rezaul Alam Chowdhury
+11.3 111103 10154 Weights and Measures Gordon Cormack
+11.4 111104 116 Unidirectional TSP Owen Astrakan
+11.5 111105 10003 Cutting Sticks Manuel Carro, Julio Mario
+11.6 111106 10261 Ferry Loading Gordon Cormack
+11.7 111107 10271 Chopsticks Rujia Liu
+11.8 111108 10201 Adventures in Moving: Part IV Gordon Cormack, Ondrej Lhotak
+12.1 111201 10161 Ant on a Chessboard Long Chong
+12.2 111202 10047 The Monocycle Shahriar Manzoor, Rezaul Alam Chowdhury
+12.3 111203 10159 Star Petko Minkov
+12.4 111204 10182 Bee Maja Rujia Liu, Ralf Engels
+12.5 111205 707 Robbery Miguel Revilla, Immanuel Herrman
+12.6 111206 10177 (2/3/4)-D Sqr/Rects/Cubes? Shahriar Manzoor
+12.7 111207 10233 Dermuba Triangle Arun Kishore
+12.8 111208 10075 Airlines Shahriar Manzoor, Rezaul Alam Chowdhury
+13.1 111301 10310 Dog and Gopher Gordon Cormack
+13.2 111302 10180 Rope Crisis in Ropeland! Shahriar Manzoor, Rezaul Alam Chowdhury
+13.3 111303 10195 Knights of the Round Table Pedro Demasi
+13.4 111304 10136 Chocolate Chip Cookies Gordon Cormack
+13.5 111305 10167 Birthday Cake Long Chong
+13.6 111306 10215 The Largest/Smallest Box ... Shahriar Manzoor
+13.7 111307 10209 Is This Integration? Shahriar Manzoor
+13.8 111308 10012 How Big Is It? Gordon Cormack
+14.1 111401 10135 Herding Frosh Gordon Cormack
+14.2 111402 10245 The Closest Pair Problem Shahriar Manzoor
+14.3 111403 10043 Chainsaw Massacre Miguel Revilla, Christoph Mueller
+14.4 111404 10084 Hotter Colder Gordon Cormack
+14.5 111405 10065 Useless Tile Packers Shahriar Manzoor, Rezaul Alam Chowdhury
+14.6 111406 849 Radar Tracking Gordon Cormack
+14.7 111407 10088 Trees on My Island Shahriar Manzoor, Rezaul Alam Chowdhury
+14.8 111408 10117 Nice Milk Rujia Liu
+'''
+
+data = u'''
+Jaroslav  Beňo      110206  2   110408  1
+Matej  Brašeň       110603  2   111105  2
+Martin  Brzula      110904  2   110902  2
+Juraj  Bubniak      110207  1   110204  2
+Ján  Fekeš      111001  2   110406  2
+Peter  Franček      110601  1   110705  2
+Ladislav  Gubala        110107  1   110106  2
+Ivan  Hraško        110908  3   110505  3
+Pavel  Jančiar      110501  1   110203  2
+Jakub  Jurčík       110805  2   110704  2
+Nikoleta  Kabáčová      110304  2   110306  2
+Peter  Kajan        110101  1   110402  2
+Ján  Maár       110305  1   110802  3
+Matúš  Marko        110607  2   111207  2
+Jozef  Medveď       110701  1   111303  2
+Tomáš  Nguyen       110103  1   110302  2
+Matúš  Novysedlák       110205  1   110208  3
+Peter  Otto     110303  1   110405  2
+Radoslav  Rajčan        110102  1   110202  2
+Matej  Tepper       110301  1   110608  2
+Ján  Turský     110502  1   110504  2
+Štefan  Ušák        110201  1   111302  2
+Martin Jančo        110702    2   110703    1
+'''
+
+
+tasks = [task for task in tasks.split('\n') if task]
+task_dict = {}
+
+
+for task in tasks:
+    splitted = task.split(' ', 3)
+
+    title = splitted[3]
+
+    if ',' in title:
+        title = title.split(',')[0]
+
+    task_dict[splitted[1]] = title
+
+
+lines = [line.split() for line in data.split(u'\n') if line]
+
+nazov = 'Nazov'
+uloha = 'Odkaz'
+obtiaznost = 'Obtiaznost'
+student = 'Student'
+
+print '|',
+print nazov.ljust(41),
+print '|',
+print uloha.ljust(18),
+print '|',
+print obtiaznost.ljust(11),
+print '|',
+print student.ljust(17),
+print '|'
+print '|',
+print 41 * '-',
+print '|',
+print 18 * '-',
+print '|',
+print 11 * '-',
+print '|',
+print 17 * '-',
+print '|'
+
+
+for name, lastname, id1, level1, id2, level2 in lines:
+    fullname = u'{0} {1}'.format(name, lastname)
+
+    print '|',
+    print task_dict[id1].ljust(41),
+    print '|',
+    print '[{0}](/{1}/)'.format(id1, id1),
+    print '|',
+    print level1.ljust(11, ' '),
+    print '|',
+    print fullname.ljust(17, ' '),
+    print '|'
+    print '|',
+    print task_dict[id2].ljust(41),
+    print '|',
+    print '[{0}](/{1}/)'.format(id2, id2),
+    print '|',
+    print level2.ljust(11, ' '),
+    print '|',
+    print fullname.ljust(17, ' '),
+    print '|'
+
+
