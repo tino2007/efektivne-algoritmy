96d3a2c407ca773bc97961f2397ac3f5ad3f4295
jbub <git@jbub.eu>
2014-05-27 13:14:47 +0200

    update 110501

--- 110501/Main.java
+++ 110501/Main.java
@@ -41,56 +41,56 @@ class Main implements Runnable{
 }
 class myStuff implements Runnable{
     public void run(){
-        	Citacka();
+            Citacka();
         }
     /**
-	 * @param args
-	 */
-	public static void Citacka(){//nacitanie udajov
-		//Scanner sc = new Scanner(System.in);
-		Integer c1;
-		Integer c2;
-		String[] temp;
-		
-		String riadok = Main.ReadLn(255);
-		temp = riadok.trim().split(" ");
-		 while (!temp[0].equals("0") && !temp[1].equals("0")){
-			//c1 = Integer.valueOf(temp[0]);
-			//c2 = Integer.valueOf(temp[1]);
-			int carry = 0;
-			int prenesenaHodnota = 0;
-			int i = 0;
-			int minLength = Math.max(temp[0].length(), temp[1].length());
-			do {
-				c1 = Integer.parseInt("" + temp[0].charAt(temp[0].length() - i - 1));
-				c2 = Integer.parseInt("" + temp[1].charAt(temp[1].length() - i - 1));
-				if ((c1 + c2 + prenesenaHodnota) >= 10)
-				{
-					carry++;
-					prenesenaHodnota = 1;
-				}
-				else 
-				{
-					prenesenaHodnota = 0;
-				}				
-				i++;
-			} while (i < minLength);
-			
-			if (carry == 0)
-				System.out.println("No carry operation.");
-			else if (carry == 1)
-				System.out.println("1 carry operation.");
-			else 
-				System.out.println(carry + " carry operations.");
+     * @param args
+     */
+    public static void Citacka(){//nacitanie udajov
+        //Scanner sc = new Scanner(System.in);
+        Integer c1;
+        Integer c2;
+        String[] temp;
 
-			
-			riadok = Main.ReadLn(255);
-			temp = riadok.trim().split(" ");
-		}
-		 return;
-	}
+        String riadok = Main.ReadLn(255);
+        temp = riadok.trim().split(" ");
+         while (!temp[0].equals("0") && !temp[1].equals("0")){
+            //c1 = Integer.valueOf(temp[0]);
+            //c2 = Integer.valueOf(temp[1]);
+            int carry = 0;
+            int prenesenaHodnota = 0;
+            int i = 0;
+            int minLength = Math.max(temp[0].length(), temp[1].length());
+            do {
+                c1 = Integer.parseInt("" + temp[0].charAt(temp[0].length() - i - 1));
+                c2 = Integer.parseInt("" + temp[1].charAt(temp[1].length() - i - 1));
+                if ((c1 + c2 + prenesenaHodnota) >= 10)
+                {
+                    carry++;
+                    prenesenaHodnota = 1;
+                }
+                else
+                {
+                    prenesenaHodnota = 0;
+                }
+                i++;
+            } while (i < minLength);
+
+            if (carry == 0)
+                System.out.println("No carry operation.");
+            else if (carry == 1)
+                System.out.println("1 carry operation.");
+            else
+                System.out.println(carry + " carry operations.");
+
+
+            riadok = Main.ReadLn(255);
+            temp = riadok.trim().split(" ");
+        }
+         return;
+    }
     // You can insert more classes here if you want.
 }
 
-// Pogram h¾adá poèet "carry" operácií. Naèíta si riadok, rozdelí pod¾a medzery a potom postupne
-// odzadu sèítava èísla. Ak je súèet viac ako 9 tak pripoèíta carry a prechádza na ïalšie èísla.
+// Pogram hlada pocet "carry" operacii. Nacita si riadok, rozdeli podla medzery a potom postupne
+// odzadu scitava cisla. Ak je sucet viac ako 9 tak pripocita carry a prechadza na dalsie cisla.
